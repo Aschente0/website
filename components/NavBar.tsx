1f09013af532d84404c7da29835346e69c9c3f68
Aschente0 <carlosyukming.lam@hotmail.com>
2024-07-15 00:57:45 -0400

    remove noscroll projects link

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,7 +5,7 @@ export default function NavBar() {
     <div className="sticky top-0 z-20 h-[6.25rem] pl-[4rem] flex items-center text-lg gap-[4rem] bg-gray-600 drop-shadow-md">
       <Link className="cursor-pointer" href="/">Home</Link>
       <Link className="cursor-pointer" href="/about">About</Link>
-      <Link className="cursor-pointer" href="/projects" scroll={false}>Projects</Link>
+      <Link className="cursor-pointer" href="/projects">Projects</Link>
       <Link className="cursor-pointer" href="/blogs">Blog</Link>
     </div>
   )
