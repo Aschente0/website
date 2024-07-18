2279212fbb805cd9dc93c5b5a8bd56391ceb5fe6
Aschente0 <carlosyukming.lam@hotmail.com>
2024-07-18 01:01:07 -0400

    improve bio section aspect ratio

--- app/page.tsx
+++ app/page.tsx
@@ -20,24 +20,27 @@ export default async function Home() {
       {/* About Section */}
       <section className={`pt-[65vh] tablet:pt-[85vh]`}> {/* padding to render below hero section */}
         <div className="bg-gray-300 relative grid grid-flow-row tablet:grid-flow-col grid-cols-2 grid-rows-2 tablet:grid-rows-none tablet:grid-cols-7 gap-8 p-8 m-8 tablet:p-10 tablet:m-0">
-          <div className="col-span-1 tablet:col-span-2 relative -ml-8 -mb-8 tablet:-ml-10 tablet:-mb-10 -mr-[12rem] min-[600px]:-mr-[2rem] min-[640px]:-mr-[8rem] min-[768px]:-mr-[6rem]">
-            <Image
-              src={homePageData.aboutSection.image}
-              alt={"Carlos Lam"}
-              className="z-0"
-              fill={true}
-              style={{
-                objectFit: "contain",
-                objectPosition: "left bottom"
-              }}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            />
+          <div className="col-span-1 tablet:col-span-2 relative -ml-8 -mb-8 tablet:-ml-10 tablet:-mb-10 -mt-10 -mr-[12rem] min-[600px]:-mr-[2rem] min-[640px]:-mr-[8rem] min-[768px]:-mr-[6rem]">
+            <div className="relative h-[40rem]">
+              <Image
+                src={homePageData.aboutSection.image}
+                alt={"Carlos Lam"}
+                className="z-0"
+                fill={true}
+                style={{
+                  objectFit: "contain",
+                  objectPosition: "left bottom"
+                }}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+            </div>
+
           </div>
-          <div className="col-span-1 tablet:col-span-2 z-10 flex flex-col justify-center items-center text-center mt-12">
+          <div className="col-span-1 tablet:col-span-2 z-10 flex flex-col justify-center items-center text-center tablet:items-start mt-12">
             <p className="text-h3 font-bold">{homePageData.aboutSection.name}</p>
             <p className="text-h6">{homePageData.aboutSection.title}</p>
           </div>
-          <div className="col-span-2 tablet:col-span-3 z-10 pt-10 tablet:pt-0 -mx-8 p-8 tablet:-mx-0 tablet:p-0 border-t-[2rem] tablet:border-t-0 border-gray-600">
+          <div className="col-span-2 tablet:col-span-3 flex flex-col justify-center z-10 pt-10 tablet:pt-0 -mx-8 p-8 tablet:-mx-0 tablet:p-0 border-t-[2rem] tablet:border-t-0 border-gray-600">
             <MarkdownRenderer content={homePageData.aboutSection.content}/>
           </div>
         </div>
