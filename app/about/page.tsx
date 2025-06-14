8487b7c550fd4f726f4298b39cd42b0d9cb2c1d2
Aschente0 <carlosyukming.lam@hotmail.com>
2025-06-14 11:49:54 -0400

    optional about experience logo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,19 +4,19 @@ import { RevalidationTag } from "@/sanity/utils";
 import { formatDateString } from "@/utils/date";
 import Image from "next/image";
 
-const ExperienceBreakdown = ({e}: {e: Experience}) => {
+const ExperienceBreakdown = ({ e }: { e: Experience }) => {
   return (
     <div>
-      { e.projects?.map((p, i) => {
+      {e.projects?.map((p, i) => {
         return (
           <div
             key={`ExperienceBreakdown${p._id}`}
             className="p-12 grid grid-flow-col grid-cols-7 gap-10"
           >
-            
+
             <div className="col-span-1 flex justify-center items-center text-h4 font-bold font-serif text-opacity-50 text-gray-900 relative">
               <span>•</span>
-              {i+1 < e.projects.length && <div className="border-l-2 border-gray-900 border-opacity-50 h-[6rem] absolute bottom-[-6rem]"></div>}
+              {i + 1 < e.projects.length && <div className="border-l-2 border-gray-900 border-opacity-50 h-[6rem] absolute bottom-[-6rem]"></div>}
             </div>
             <div className="relative col-span-2 tablet:col-span-1">
               <Image
@@ -40,12 +40,12 @@ const ExperienceBreakdown = ({e}: {e: Experience}) => {
   )
 }
 
-const ExperienceComponent = ({e}: {e: Experience}) => {
+const ExperienceComponent = ({ e }: { e: Experience }) => {
   return (
     <div>
       <div className="bg-gray-300 p-10 grid grid-flow-col grid-cols-7 gap-16 mt-8 tablet:mt-16">
         <div className="relative col-span-2 tablet:col-span-1">
-          <Image
+          {e.condensedLogo && <Image
             src={e.condensedLogo}
             alt={e.name}
             fill={true}
@@ -53,7 +53,7 @@ const ExperienceComponent = ({e}: {e: Experience}) => {
               objectFit: "contain"
             }}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          />
+          />}
         </div>
         <div className="col-span-5 tablet:col-span-6">
           <p className="text-h5 font-bold">{e.name}</p>
@@ -67,32 +67,32 @@ const ExperienceComponent = ({e}: {e: Experience}) => {
   )
 }
 
-const PartnershipComponent = ({p}: {p: Partnership}) => {
+const PartnershipComponent = ({ p }: { p: Partnership }) => {
   return (
     <div className="bg-gray-300 p-16 grid grid-flow-col grid-cols-7 gap-16 mt-16">
-    <div className="relative col-span-2 tablet:col-span-1">
-      <Image
-        src={p.fullLogo}
-        alt={p.name}
-        fill={true}
-        style={{
-          objectFit: "contain"
-        }}
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-      />
+      <div className="relative col-span-2 tablet:col-span-1">
+        <Image
+          src={p.fullLogo}
+          alt={p.name}
+          fill={true}
+          style={{
+            objectFit: "contain"
+          }}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
+      </div>
+      <div className="col-span-5 tablet:col-span-6">
+        <p className="text-h4 font-bold">{p.name}</p>
+        <p className="text-lg">{p.description}</p>
+      </div>
     </div>
-    <div className="col-span-5 tablet:col-span-6">
-      <p className="text-h4 font-bold">{p.name}</p>
-      <p className="text-lg">{p.description}</p>
-    </div>
-  </div>
   )
 }
 
 export default async function Projects() {
-  const {experience, partnerships} = await client.fetch<ExperienceAndPartnershipsQuery>(experienceAndPartnershipsQuery,
+  const { experience, partnerships } = await client.fetch<ExperienceAndPartnershipsQuery>(experienceAndPartnershipsQuery,
     {},
-    { next: { tags: [RevalidationTag.experience, RevalidationTag.partnership] }});
+    { next: { tags: [RevalidationTag.experience, RevalidationTag.partnership] } });
   return (
     <main className="min-h-[85vh] m-8 tablet:m-0">
       <h1 className="text-h4 font-bold">Experience</h1>
